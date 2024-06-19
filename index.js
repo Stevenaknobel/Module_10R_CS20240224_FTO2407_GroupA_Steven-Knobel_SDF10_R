85814f0910ba6e6b22c0f3a6573ad0dc87416a8c
Muneer Abrahams <audipunkwp@gmail.com>
2024-06-19 13:09:37 +0200

    update

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 
 import { initializeApp } from "https://real-time-database-5c238-default-rtdb.firebaseio.com/"
-import { getDatabase } from "https://real-time-database-5c238-default-rtdb.firebaseio.com/"
+import { getDatabase, ref, push } from "https://real-time-database-5c238-default-rtdb.firebaseio.com/"
 
+const appSettings = {
+    databaseURL: "https://real-time-database-5c238-default-rtdb.firebaseio.com/"
+}
 
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
-addButtonEl.addEventListener
-
-(
-    "click", function()
+addButtonEl.addEventListener("click", function()
  {
     let inputValue = inputFieldEl.value
-    
+
+     push(shoppingListInDB, inputValue)
+      
     console.log(inputValue)
 }
 )
