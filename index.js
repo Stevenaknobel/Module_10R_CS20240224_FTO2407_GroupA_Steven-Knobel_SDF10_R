a327678fbe041eed143e7ed1c1521ff371b91317
Steven <stevenaknobel@gmail.com>
2024-08-01 14:35:41 +0200

    set up array and list for the shopping, set up CSS with flex

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ Make it so that when you click the 'Add to cart' button, whatever is written in 
 */
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-35cfe-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -16,11 +16,34 @@ const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-
+    
     push(shoppingListInDB, inputValue)
     
-    console.log(inputValue)
+    clearInputFieldEl()
 })
+
+onValue(shoppingListInDB, function(snapshot) {
+    let itemsArray = Object.values(snapshot.val())
+
+    clearShoppingListel()
+
+    for (let i = 0; i < itemsArray.length; i++) {
+        appendItemToShoppingListEl(itemsArray[i])
+    }
+})
+
+function clearShoppingListel() {
+    shoppingListEl.innerHTML = ""
+}
+
+function clearInputFieldEl() {
+    inputFieldEl.value = " "
+}
+
+function appendItemToShoppingListEl(itemValue) {
+       shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
